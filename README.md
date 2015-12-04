b0a23cab96439fdd6c4b0cecac00413dad72949d
linfk <linfk@ucweb.com>
2015-12-04 12:01:44 +0800

    a

---
+++ README.md
@@ -0,0 +1,18 @@
+### sname
+```
+文件头部插入签名
+自定义签名方式为项目根目录下放置签名模板文件：_signature.txt
+```
+
+### 安装
+```
+需要nodeJS环境。
+npm install -g sname
+```
+
+### 使用
+```
+单文件 sname test.html
+多文件 sname test.html test.js test.css
+目录下所有文件 sname dir/
+```
---
+++ index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+var fs = require('fs'),
+    path = require('path'),
+    buffer = require('buffer').Buffer;
+
+var signCont = null,
+    signPath = '_signature.txt', //自定义签名
+    targetList = process.argv;
+
+fs.exists(signPath, function (bo) {
+    signCont = bo ? rFile(signPath) : rFile(path.join(__dirname, 'signature_def.txt'));
+    for (var i = 2; i < targetList.length; i++) {
+        var filePath = targetList[i],
+            stat = fs.statSync(filePath);
+        if (stat.isFile()) {
+            actFile(filePath, signCont);
+        }
+        if (stat.isDirectory()) {
+            var list = fs.readdirSync(filePath);
+            list.forEach(function (item) {
+                actFile(path.join(filePath, item), signCont);
+            });
+        }
+    }
+});
+
+function rFile(path) {
+    return fs.readFileSync(path, {
+        encoding: "utf8"
+    });
+}
+
+function getSuffix(path) {
+    var arr = path.split('.');
+    return arr[arr.length - 1];
+}
+
+function notes(cont, path) {
+    var suffix = getSuffix(path);
+    if (suffix === 'html') {
+        return '<!--\r' + cont + '\r-->\r';
+    } else {
+        return '/*\r' + cont + '\r*/\r';
+    }
+}
+
+function actFile(filePath, signCont) {
+    fs.readFile(filePath, function (err, data) {
+        var finalResult = buffer.concat([new buffer(notes(signCont, filePath)), data]);
+        fs.unlinkSync(filePath);
+        fs.writeFileSync(filePath, finalResult);
+        process.stdout.write('done\r\n');
+    });
+}
